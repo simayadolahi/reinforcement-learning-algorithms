32380905762f4c590e980d7994db4f9f1f8302aa
sima <sima.yadolahi@gmail.com>
2024-12-21 11:01:16 +0330

    epsilon_greedy.py changed to bandit.py

---
+++ bandit/bandit.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Bandit():
+    q, Q, n = 0 
+    def __init__(self, q, Q):
+        pass
+     
+    def __str__(self):
+        pass
+
+    def pull(self):
+        pass
+    
+
+    def update(self):
+        pass
